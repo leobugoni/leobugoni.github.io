ccac8c3ab5292f8989d53a6526c9c0e97204f2f6
Leonardo Bugoni <leobugoni@gmail.com>
2018-07-27 11:16:31 -0300

    Iserir Pessoa.js

---
+++ model/pessoa.js
@@ -0,0 +1,12 @@
+class Pessoa {
+  constructor(nome, email){
+    this.nome = nome;
+    this.email = email;
+  }
+  get getNome() {
+    return this.nome  
+  } 
+  get getEmail() {
+    return this.email  
+  }  
+}
